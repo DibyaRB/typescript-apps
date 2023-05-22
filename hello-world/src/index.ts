4d2f84a29e16efe6c167d45f715d7e2118c526f7
DibyaRB <dibyabhoidibya@gmail.com>
2023-05-22 12:09:34 +0530

    Using tuples and enums

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -5,4 +5,20 @@ let coure:string = "TypeScript";
 
 let is_published:boolean = true;
 
-let level; // typescript assumes this value is of type any
+let level; // typescript assumes this value is of type any 
+
+function render(document: any){
+    console.log(document);
+}
+
+let numbers:number[] = [1,2,3];
+
+numbers.forEach(n=>n.toLocaleString);
+
+//Tuples : Internally they are just normal JS Arrays
+
+let user:[Number, string]  = [1, 'Dibya'];
+
+//ENUMS
+
+const enum Size { Small = 0, Medium, Large}; //Mediume gets value as 1. Large as 2

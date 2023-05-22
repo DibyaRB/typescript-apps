8580161e21f48c00cda0fbe2d56a2d2a1ed0c346
DibyaRB <dibyabhoidibya@gmail.com>
2023-05-22 11:55:37 +0530

    Using any

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,3 +1,8 @@
-let age: number = 20;
-if(age< 50)
-age+=10;
+//We don't have to annotate everytime
+
+let sales:number = 123_456_789;
+let coure:string = "TypeScript";
+
+let is_published:boolean = true;
+
+let level; // typescript assumes this value is of type any
